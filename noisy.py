Fixed calculation method and added gain

noisy.py
@@ -3,38 +3,45 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Reading CSV file
-data = pd.read_csv("photodiode_measured_electronic_power_spectral_density.csv")
+data = pd.read_csv("10102024-Measure-DANL-with-Photodiode-on-blocked-Trace.csv")
  
 # Converting column data to list then array
 frequencies = np.array(data['[Hz]'].tolist())
 measured_electronic_power_spectral_density = np.array(data['Trace1[dBm]'].tolist())
-measured_electronic_power_spectral_density_corrected = measured_electronic_power_spectral_density + 19  # measurements were with -19 dBm reference level
-
+#measured_electronic_power_spectral_density_corrected = measured_electronic_power_spectral_density  # measurements were with -19 dBm reference level
 
 responsivity = 1.04 # [A/W]
-nep = 1.2e-11       # [W/sqrt(Hz)]
+nep = 1.2e-11       # [W/sqrt(Hz)] optical power
+gain = 1e4          # [V/A]
+r = 50              # [Ohm]
 
 # noise equivalent electronic amplitude
-neea = nep * responsivity
+neea = nep * responsivity * gain    # [V/sqrt(Hz)]
 
 # square neea and use P=R*I^2
-r = 50 # [Ohm]
-electronic_power_spectral_density = (neea) * r
-print(electronic_power_spectral_density)
+electronic_power_spectral_density = (neea ** 2) / r # [W/Hz]
+
+# calculate electronic power spectral density per 10 kHz = RBW
+electronic_power_spectral_density_per_10kHz = electronic_power_spectral_density * 1e4
+
 # calculate electronic power spectral density in dBm
-electronic_power_spectral_density_dBm = 10 * np.log10(electronic_power_spectral_density) + 30
+electronic_power_spectral_density_per_10kHz_dBm = 10 * np.log10(electronic_power_spectral_density_per_10kHz * 1000)
 
+print('The NEP-calculated electronic power spectral density per 10 kHz is: ', electronic_power_spectral_density_per_10kHz_dBm, ' dBm')
+
+
+# Make const. array to display NEP-calculated PSD
 f = np.linspace(0, 1600000000, 1600)
-electronic_power_spectral_density_dBm_array = np.full(1600, electronic_power_spectral_density_dBm)
-#print(electronic_power_spectral_density_array)
+electronic_power_spectral_density_per_10kHz_dBm_array = np.full(1600, electronic_power_spectral_density_per_10kHz_dBm)
 
+
+# Plot Measured PSD vs. NEP calculated PSD
 plt.figure()
-plt.plot(f, electronic_power_spectral_density_dBm_array)  # 'o', markersize=2
-plt.plot(frequencies, measured_electronic_power_spectral_density_corrected)
-#plt.xlim(-1e-12, 1e-12)
-#plt.title('Time domain electric field')
-#plt.xlabel('Time [s]')
-#plt.ylabel('Electric field')
+plt.plot(f, electronic_power_spectral_density_per_10kHz_dBm_array)
+plt.plot(frequencies, measured_electronic_power_spectral_density)
+plt.xlim(1e5, 1.6e9)
+plt.title('Measured PSD vs. NEP-calculated PSD')
+plt.xlabel('Frequency [Hz]')
+plt.ylabel('Electronic power spectral density [dBm]')
+plt.legend(['Measured PSD','NEP-calculated PSD'])
 plt.show()
-
-# FEHLER IN MAXIS EINTRAG: HAT NUR EINHEITEN QUADRIERT, NICHT WERTE BEI 1e-10 A^2/Hz