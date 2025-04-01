e62395bb0e833513f6b493aefeb4b5f43b172d8e
Tim Schoenwald <timschoenwald@googlemail.com>
2025-04-01 18:26:18 +0200

    Added log output calculation and curve to bhd sensitivity plot

--- bhd-filter-series.py
+++ bhd-filter-series.py
@@ -1,8 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import constants
+from matplotlib.ticker import LogLocator
 
-# Load CSV data
+###### Load CSV data #####
+
+# Balanced
 file_nofilter = "E:/Measurements/46/2025-03-24/nofilter.csv"
 file_nd10a = "E:/Measurements/46/2025-03-24/nd10a.csv"
 file_nd20a = "E:/Measurements/46/2025-03-24/nd20a.csv"
@@ -12,17 +15,33 @@ file_nd40a_ne20ab = "E:/Measurements/46/2025-03-24/nd40a-ne20ab.csv"
 file_nd40a_ne10ab_ne20ab = "E:/Measurements/46/2025-03-24/nd40a-ne10ab-ne20ab.csv"
 file_nd40a_nenir20ac_ne20ab = "E:/Measurements/46/2025-03-24/nd40a-nenir20ac-ne20ab.csv"
 file_nd40a_nenir20ac_ne20ab_ne10ab = "E:/Measurements/46/2025-03-24/nd40a-nenir20ac-ne20ab-ne10ab.csv"
-data = np.array([])
-
+data_bal = np.array([])
 for i in [file_nofilter, file_nd10a, file_nd20a, file_nd40a, file_nd40a_ne10ab, file_nd40a_ne20ab, file_nd40a_ne10ab_ne20ab, file_nd40a_nenir20ac_ne20ab, file_nd40a_nenir20ac_ne20ab_ne10ab]:
-    data = np.append(data, np.mean(np.genfromtxt(i, delimiter=",", skip_header=21)[:,1]))
+    data_bal = np.append(data_bal, np.mean(np.genfromtxt(i, delimiter=",", skip_header=21)[:,1]))
 
-r = 0.67                 # Photodetector responsivity (A/W)
-g = 100e3                # V/A
+# Log
+file_log_ND40A_low = "E:/Measurements/46/2025-03-31/log-ND40A-low.csv"
+file_log_ND40A_high = "E:/Measurements/46/2025-03-31/log-ND40A-high.csv"
+file_log_ND40A_NE10AB_low = "E:/Measurements/46/2025-03-31/log-ND40A-NE10AB-low.csv"
+file_log_ND40A_NE10AB_high = "E:/Measurements/46/2025-03-31/log-ND40A-NE10AB-high.csv"
+file_log_ND40A_NE20AB_low = "E:/Measurements/46/2025-03-31/log-ND40A-NE20AB-low.csv"
+file_log_ND40A_NE20AB_high = "E:/Measurements/46/2025-03-31/log-ND40A-NE20AB-high.csv"
+data_log = np.array([])
+data_log_span = np.array([])
+for i in [file_log_ND40A_low, file_log_ND40A_high, file_log_ND40A_NE10AB_low, file_log_ND40A_NE10AB_high, file_log_ND40A_NE20AB_low, file_log_ND40A_NE20AB_high]:
+    data_log = np.append(data_log, np.mean(np.genfromtxt(i, delimiter=",", skip_header=21)[:,1]))
+data_log_span = np.append(data_log_span, [np.abs(data_log[1] - data_log[0]), np.abs(data_log[3] - data_log[2]), np.abs(data_log[5] - data_log[4])])
+""" data = np.append(np.abs(np.mean(file_log_ND40A_high) - np.mean(file_log_ND40A_low)))
+data = np.append(np.abs(np.mean(file_log_ND40A_NE10AB_high) - np.mean(file_log_ND40A_NE10AB_low)))
+data = np.append(np.abs(np.mean(file_log_ND40A_NE20AB_high) - np.mean(file_log_ND40A_NE20AB_low))) """
+print(data_log_span)
+
+r = 0.67                # Photodetector responsivity (A/W)
+g = 100e3               # V/A
 lo_power = 0.1975e-3    # W
-data_power = data / (g * r)  # Maybe factor 2 because of Interferogram peak to peak
-data_voltage = data
 lo_voltage = g * r * lo_power 
+#data_power = data / (g * r)  # Maybe factor 2 because of Interferogram peak to peak
+data_voltage = data_bal
 transmission = np.array([1, 0.1104, 0.01190, 0.0003510, 0.0003510*0.0791, 0.0003510*0.02482, 0.0003510*0.0791*0.02482, 0.0003510*0.009257*0.02482, 0.0003510*0.009257*0.02482*0.1104])
 signal_power = transmission * lo_power
 signal_voltage = transmission * lo_voltage
@@ -33,13 +52,25 @@ expected = 4 * np.sqrt(x_axis) * np.sqrt(lo_power) #/ (constants.epsilon_0 * con
 expected_voltage = g * r * expected
 #theory = 4 * x_axis * lo_voltage
 #theory = 4 * x_axis * lo_voltage
-print(4 * np.sqrt(2e-4) * np.sqrt(2e-4))
-print(lo_power*4*g*r)
+#print(4 * np.sqrt(2e-4) * np.sqrt(2e-4))
+#print(lo_power*4*g*r)
+
+lo_power_log = 0.2e-3
+transmission_log = np.array([0.0003510, 0.0003510*0.0791, 0.0003510*0.02482])
+signal_power_log = transmission_log * lo_power_log
 
 wavelength = 1064e-9
+frequency = constants.c / wavelength
 shot_noise = np.sqrt(2 * constants.h * constants.c / (wavelength * 2 * lo_power))
 one_photon = constants.h * constants.c / wavelength
-v_shot = g * np.sqrt(2 * constants.elementary_charge * lo_power * r)
+v_shot = g * np.sqrt(2 * constants.elementary_charge * lo_power * r)    # 1*lo_power because shot_noise level is relevant at low signal arm powers only
+photons_shot = v_shot / (constants.h * frequency)
+
+E_photon = constants.h * frequency
+# Function to convert Power (W) to Photons per second
+def power_to_photons(P):
+    return P / E_photon
+
 #print(theory)
 # Plot the results
 #plt.semilogy(frequencies, psd_balanced_400_sqrt, label='Balanced 400 mW')
@@ -57,12 +88,14 @@ ax1.loglog(signal_power, data)
 plt.show() """
 
 ####### add labels etc.
-plt.figure(figsize=(10, 6))
+#plt.figure(figsize=(10, 6))
+fig, ax1 = plt.subplots(figsize=(8,6))
 #plt.loglog(x_axis_voltage, expected_voltage)
-plt.loglog(x_axis, expected_voltage, label='g*r*4*Es*ELO')
+#plt.loglog(x_axis, expected_voltage, label='g*r*4*Es*ELO')
 plt.gca().invert_xaxis()  # Inverts the x-axis
 #plt.loglog(signal_voltage, data)
-plt.loglog(signal_power, data_voltage, '--bo', label='Measured Signal')
+plt.loglog(signal_power, data_bal, '--o',color="green", label='Measured Signal Balanced (Linear Output)')
+plt.loglog(signal_power_log, data_log_span, '--o',color="blue" , label='Measured Signal Autobalanced (Log Output)')
 plt.axhline(v_shot, color='r', linestyle='--', label="Shot Noise")
 plt.axvline(one_photon, color='black', linestyle='--', label="One Photon/s")
 plt.xlim(1e-2, 1e-21)
@@ -70,7 +103,21 @@ plt.ylim(1e-7, 2)
 plt.xlabel('Signal Arm Power [W]')
 plt.ylabel('Measured Signal [V]')
 plt.title('BHD Signal vs. Signal Arm Power')
-plt.legend()
+plt.legend(loc='lower left', bbox_to_anchor=(0,0.2))
+ax1.xaxis.grid(visible=True, which='both')
+ax1.yaxis.grid(visible=True, which='major')
+
+ax2 = ax1.secondary_xaxis("top", functions=(power_to_photons, lambda N: N * E_photon))  # transform function and its inverse
+ax2.set_xlabel("Signal Arm [Photons/s]")
+
+""" # Set log tick locators explicitly to ensure minor ticks appear
+ax1.xaxis.set_major_locator(LogLocator(base=10.0, subs=None))  # Major ticks
+ax1.xaxis.set_minor_locator(LogLocator(base=10.0, subs=np.arange(2, 10) * 0.1, numticks=10))  # Minor ticks
+
+ax1.yaxis.set_major_locator(LogLocator(base=10.0, subs=None))  # Major ticks for y
+ax1.yaxis.set_minor_locator(LogLocator(base=10.0, subs=np.arange(2, 10) * 0.1, numticks=10)) """
+
+
 """ 
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Power (dBm)")
